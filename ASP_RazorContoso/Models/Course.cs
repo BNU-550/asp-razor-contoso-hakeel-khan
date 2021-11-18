488f355baff3d488facef00b05ee249e03227c90
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-18 14:51:02 +0000

    classes

---
+++ ASP_RazorContoso/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_RazorContoso.Models
+{
+    
+        public class Course
+        {
+            [DatabaseGenerated(DatabaseGeneratedOption.None)]
+            public int CourseID { get; set; }
+            public string CourseCode { get; set; }
+            public string Title { get; set; }
+            public int Credits { get; set; }
+
+            // navigation properties
+            public virtual ICollection<Enrollment> Enrollments { get; set; }
+        }
+}
+
---
+++ ASP_RazorContoso/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_RazorContoso.Models
+{
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public DateTime EnrollmentDate { get; set; }
+
+            //Relationships or navigation properties
+            public virtual ICollection<Enrollment> Enrollments { get; set; }
+        }
+    }
+}
